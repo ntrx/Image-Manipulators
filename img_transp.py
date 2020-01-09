7e8a07b82c2cca621b76420c3c401ca488ec6f3e
Bohdan <ntrxhm@gmail.com>
2020-01-09 11:45:35 +0200

    Auto converting all png files in current dir
    Auto convert all png files in current directory (additionally switchable converting to alpha channel white colour)

--- img_transp.py
+++ img_transp.py
@@ -8,89 +8,90 @@ import paramiko
 import os
 import struct
 
-# source for non-transparent file
-img_source = 'headquarters_unit'
-# Make this file transparent (white to alpha channel), True if yes.
-go_transparent = True
+# server settings
+bs_host = '127.0.0.1'
+bs_user = 'ntrx'
+bs_pass = '1111'
+bs_port = 22
 
 # format settings
 img_format = '.png'
 rgba_format = '.smb'
 
+# Convert white background into alpha channel in PNG files
+go_transparent = True
 
 
-# server settings
-bs_host = '192.168.9.111'
-bs_user = 'ntrx'
-bs_pass = '1111'
-bs_port = 22
+filelist = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in filelist:
+    if f.find(img_format) >= 0:
+
+        img_source = f
+        img_output = img_source
+        img_upload = img_output
+        smb_output = img_source.replace(img_format, rgba_format)
+
+        paramiko.util.log_to_file('history.log')
 
 
-img_output = img_source
+        if go_transparent:
+            img = Image.open(img_upload)
+            img = img.convert("RGBA")
+            datas = img.getdata()
 
-img_upload = img_output + img_format
-if not go_transparent:
-    smb_output = img_source + rgba_format
-else:
-    smb_output = img_source + '_a' + rgba_format
-        
+            newData = []
+            for item in datas:
+                if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                    newData.append((255, 255, 255, 0))
+                else:
+                    newData.append(item)
 
-paramiko.util.log_to_file('history.log')
+            img.putdata(newData)
+            img.save(img_source, "PNG")
 
-
-if go_transparent:
-    img = Image.open(img_upload)
-    img = img.convert("RGBA")
-    datas = img.getdata()
-
-    newData = []
-    for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
-            newData.append((255, 255, 255, 0))
-        else:
-            newData.append(item)
-
-    img.putdata(newData)
-    img.save(img_source + '_a' + img_format, "PNG")
-
-if go_transparent:
-    img_output = img_source + '_a' + img_format
+        if go_transparent:
+            img_output = img_source
 
 
-im = Image.open(img_output)
-width, height = im.size
+        im = Image.open(img_output)
+        width, height = im.size
 
 
 
-print(width, height)
+        # delete exists file
+        if os.path.isfile(smb_output):
+            os.remove(smb_output)
+            
+        while not os.path.exists(smb_output):
+            print('%s => %s [w=%d, h=%d]' % (img_source, smb_output, width, height))
+            if os.path.exists(smb_output) and os.path.getsize(smb_output) == 0:
+                break
+            # put file to server
+            transport = paramiko.Transport(bs_host, bs_port)
+            transport.connect(username=bs_user, password=bs_pass)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            sftp.put(localpath=os.getcwd()+'\\'+img_output, remotepath='//home//' + bs_user + '//' + img_output)
+
+            # convert file on server
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(bs_host, bs_port, bs_user, bs_pass)
+            client.exec_command("convert %s rgba:%s" % (img_output, smb_output))
+            client.close()
+
+            # get back file
+            sftp.get(remotepath='//home//' + bs_user + '//' + smb_output, localpath=os.getcwd() + '\\' + smb_output)
+            sftp.close()
 
 
-# put file to server
-transport = paramiko.Transport(bs_host, bs_port)
-transport.connect(username=bs_user, password=bs_pass)
-sftp = paramiko.SFTPClient.from_transport(transport)
-sftp.put(localpath=os.getcwd()+'\\'+img_output, remotepath='//home//' + bs_user + '//' + img_output)
-
-# convert file on server
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(bs_host, bs_port, bs_user, bs_pass)
-client.exec_command("convert %s rgba:%s" % (img_output, smb_output))
-client.close()
-
-# get back file
-sftp.get(remotepath='//home//' + bs_user + '//' + smb_output, localpath=os.getcwd() + '\\' + img_source + rgba_format)
-sftp.close()
-
-
-# put size to file header
-f = open(img_source + rgba_format, "rb+")
-byte_arr = struct.pack('<i', int(width))
-f.seek(0)
-f.write(byte_arr)
-byte_arr = struct.pack('<i', int(height))
-f.seek(4)
-f.write(byte_arr)
-f.close()
+        # put size to file header
+        f = open(smb_output, "rb+")
+        byte_arr = struct.pack('<i', int(width))
+        f.seek(0)
+        f.write(byte_arr)
+        byte_arr = struct.pack('<i', int(height))
+        f.seek(4)
+        f.write(byte_arr)
+        f.close()
 
 
