b71af84699987a27b0d015205df0d61d7240dc88
Bohdan <ntrxhm@gmail.com>
2023-04-20 15:28:47 +0300

    Create decolorize.py
    Image decolorization processing

---
+++ decolorize.py
@@ -0,0 +1,43 @@
+from images import Image
+
+def invert2(image):
+    for y in range(image.getHeight()):
+        for x in range(image.getWidth()):
+            (r, g, b) = image.getPixel(x, y)
+            r = 255 - r
+            g = 255 - g
+            b = 255 - b
+            lum = r + g +b
+           image.setPixel(x, y, (r, g, b))
+
+def invert(image):
+    """Inverts an image to its negative."""
+    for y in range(image.getHeight()):
+        for x in range(image.getWidth()):
+            (r, g, b) = image.getPixel(x, y)
+            average = (255-r+255-g+255-b)//3
+            image.setPixel(x,y, (average, average, average))
+
+def blackAndWhite(image):
+    """Converts an image to black and white."""
+    blackPixel = (0, 0, 0)
+    whitePixel = (255, 255, 255)
+    for y in range(image.getHeight()):
+        for x in range(image.getWidth()):
+            (r, g, b) = image.getPixel(x, y)
+            average = (r + g + b) // 3
+            if average < 128:
+                image.setPixel(x, y, blackPixel)
+            else:
+                image.setPixel(x, y, whitePixel)
+
+def grayscale(image):
+    """Converts an image to grayscale."""
+    for y in range(image.getHeight()):
+        for x in range(image.getWidth()):
+            (r, g, b) = image.getPixel(x, y)
+            r = int(r * 0.299)
+            g = int(g * 0.587)
+            b = int(b * 0.114)
+            lum = r + g + b
+            image.setPixel(x, y, (lum, lum, lum))     
